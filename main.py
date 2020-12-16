e2dc1dbeba168c91bb273234b73a3c6158865b00
Luis <amail@mail.com>
2020-12-16 11:52:43 +0000

    Scrapping different types of html tables with pandas

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import time
 
-from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.support.wait import WebDriverWait
 
 FIREFOXDRIVER_PATH = "/usr/bin/"
 options = FirefoxOptions()
@@ -17,20 +15,38 @@ driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abr
 
 elem = driver.find_element_by_xpath("//input[@id='input-append-demo']")
 
+countries = ["Brazil", "South Africa", "Portugal", "Chile", "Iceland", "China", "Madagascar"]
+country = countries[1]
+
 elem.clear()
-elem.send_keys("portugal")
+elem.send_keys(country)
 elem.send_keys(Keys.RETURN)
 
-# WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "input.input-append-demo"))).send_keys("portugal")
-# # driver.find_element_by_css_selector("a.desktop-submit").click()
-# elem.send_keys(Keys.RETURN)
-# # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h1.title-title")))
-# print(driver.current_url)
+time.sleep(4)
 
+if country == "South Africa":
+    elem2 = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/div/div/div/div[3]/section[2]/article[1]/section[2]/table")
+    data = elem2.get_attribute("outerHTML")
 
-print(elem.text, elem)
-time.sleep(2)
-elem2 = driver.find_element_by_xpath("//h1[@class='margin-top1']")
-print(elem2.text)
+    df = pd.read_html(data)
+    dfs = pd.concat(df)
 
-driver.close()
+    print(f"Out of allowance rates for {country}:")
+    print(f"Calling back to the UK: {dfs.values[0][1]}")
+    print(f"Texting back to the UK: {dfs.values[2][1]}")
+    print(f"Receiving a call: {dfs.values[4][1]}")
+    print(f"Using internet data: {dfs.values[6][1]}")
+
+else:
+    elem2 = driver.find_element_by_xpath("//div[1]/section[2]/div[2]/div/table")
+    data = elem2.get_attribute("outerHTML")
+
+    df = pd.read_html(data)
+    dfs = pd.concat(df)
+    print(dfs.values[0][1])
+
+    print(f"Out of allowance rates for {country}:")
+    print(f"Calling back to the UK: {dfs.values[0][2]}")
+    print(f"Texting back to the UK: {dfs.values[2][2]}")
+    print(f"Receiving a call: {dfs.values[5][2]}")
+    print(f"Using internet data: {dfs.values[6][2]}")
