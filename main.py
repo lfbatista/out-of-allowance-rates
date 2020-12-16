85ac89cd5a770aaa37499be11f357ed80a416ecf
Luis <amail@mail.com>
2020-12-16 13:04:08 +0000

    Code refactored: functions search_country and get_allowance_rates added

--- main.py
+++ main.py
@@ -1,71 +1,65 @@
 import time
-
 import pandas as pd
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-FIREFOXDRIVER_PATH = "/usr/bin/"
+COUNTRIES = ["South Africa", "Brazil", "South Africa", "Portugal", "Chile", "Iceland", "China", "Madagascar"]
+# FIREFOXDRIVER_PATH = "/usr/bin/"
+
+# Selenium Configs
 options = FirefoxOptions()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
-
+options.add_argument('--headless')
+# options.add_argument('--disable-gpu')
 driver = Firefox(options=options)
-driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abroad")
 
-elem = driver.find_element_by_xpath("//input[@id='input-append-demo']")
 
-countries = ["Brazil", "South Africa", "Portugal", "Chile", "Iceland", "China", "Madagascar"]
-country = countries[0]
+def search_country(country):
+    driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abroad")
+    search_bar = driver.find_element_by_xpath("//input[@id='input-append-demo']")
 
-elem.clear()
-elem.send_keys(country)
-elem.send_keys(Keys.RETURN)
+    search_bar.clear()
+    search_bar.send_keys(country)
+    search_bar.send_keys(Keys.RETURN)
 
-time.sleep(5)
 
-for i, country in enumerate(countries):
-    if country == "South Africa" or country == "China" or country == "Madagascar":
-        print(i, country)
-        elem2 = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/div/div/div/div[3]/section[2]/article[1]/section[2]/table")
-        data = elem2.get_attribute("outerHTML")
+def get_allowance_rates(path):
+    """
+    :param path: xpath
+    :return: list of table values
+    """
+    table_ele = driver.find_element_by_xpath(path)
 
-        df = pd.read_html(data)
-        dfs = pd.concat(df)
+    table_data = table_ele.get_attribute("outerHTML")
+    df = pd.read_html(table_data)
 
-        print(f"Out of allowance rates for {country}:")
-        print(f"Calling back to the UK: {dfs.values[0][1]}")
-        print(f"Texting back to the UK: {dfs.values[2][1]}")
-        print(f"Receiving a call: {dfs.values[4][1]}")
-        print(f"Using internet data: {dfs.values[6][1]}")
+    return pd.concat(df)
 
-        driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abroad")
 
-        elem = driver.find_element_by_xpath("//input[@id='input-append-demo']")
-        elem.clear()
-        elem.send_keys(countries[i + 1])
-        elem.send_keys(Keys.RETURN)
+# TODO
+#  - save country allowance rates into dictionary
+#  - move out prints from loop
+#  - add logger
+for i, country in enumerate(COUNTRIES):
+    search_country(COUNTRIES[i])
+    time.sleep(4)
 
-        time.sleep(5)
+    if COUNTRIES[i] in ("South Africa", "China", "Madagascar"):
+        dfs = get_allowance_rates("/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/div/div/div/div["
+                                  "3]/section[2]/article[1]/section[2]/table")
+
+        print(f"Out of allowance rates for {country}: \n"
+              f"Calling back to the UK: {dfs.values[0][1]} \n"
+              f"Texting back to the UK: {dfs.values[2][1]} \n"
+              f"Receiving a call: {dfs.values[4][1]} \n"
+              f"Using internet data: {dfs.values[6][1]}\
+            ")
     else:
-        print(i, country)
-        elem2 = driver.find_element_by_xpath("//div[1]/section[2]/div[2]/div/table")
-        data = elem2.get_attribute("outerHTML")
+        dfs = get_allowance_rates("//div[1]/section[2]/div[2]/div/table")
 
-        df = pd.read_html(data)
-        dfs = pd.concat(df)
-
-        print(f"Out of allowance rates for {country}:")
-        print(f"Calling back to the UK: {dfs.values[0][2]}")
-        print(f"Texting back to the UK: {dfs.values[2][2]}")
-        print(f"Receiving a call: {dfs.values[5][2]}")
-        print(f"Using internet data: {dfs.values[6][2]}")
-
-        driver.get("http://www.three.co.uk/Support/Roaming_and_international/Roaming_abroad")
-
-        elem = driver.find_element_by_xpath("//input[@id='input-append-demo']")
-        elem.clear()
-        elem.send_keys(countries[i + 1])
-        elem.send_keys(Keys.RETURN)
-
-        time.sleep(5)
+        print(f"Out of allowance rates for {country}: \n"
+              f"Calling back to the UK: {dfs.values[0][2]} \n"
+              f"Texting back to the UK: {dfs.values[2][2]} \n"
+              f"Receiving a call: {dfs.values[5][1]} \n"
+              f"Using internet data: {dfs.values[6][1]}\
+            ")
